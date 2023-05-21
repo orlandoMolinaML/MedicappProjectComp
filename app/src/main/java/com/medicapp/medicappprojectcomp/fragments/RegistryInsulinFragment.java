3d6cb050d54a9b961dc8324dd7cf4676890c5ded
corinviracacha <jazminv181@hotmail.com>
2023-05-21 14:55:37 -0500

    Ajustes de estilo

--- app/src/main/java/com/medicapp/medicappprojectcomp/fragments/RegistryInsulinFragment.java
+++ app/src/main/java/com/medicapp/medicappprojectcomp/fragments/RegistryInsulinFragment.java
@@ -2,19 +2,21 @@ package com.medicapp.medicappprojectcomp.fragments;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.medicapp.medicappprojectcomp.R;
 import com.medicapp.medicappprojectcomp.databinding.FragmentRegistryInsulinBinding;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,47 +25,52 @@ import java.util.Calendar;
  */
 public class RegistryInsulinFragment extends Fragment {
     FragmentRegistryInsulinBinding binding;
-    Calendar calendar;
-    Integer year;
-    Integer month;
-    Integer day;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    private void loadDatePicker() {
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this.getContext(),
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        binding.dateEditText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
-                    }
-                }, year, month, day);
-        datePickerDialog.show();
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentRegistryInsulinBinding.inflate(inflater);
+        addCalendatInputText();
+        addTimeCalendar();
         return binding.getRoot();
     }
+
+    private void addTimeCalendar() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
+
+
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextInputLayout textDate=binding.dateTextInput;
-        calendar=Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        textDate.setEndIconOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadDatePicker();
-            }
-        });
+
     }
+
+    private void addCalendatInputText() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialogBirthday = new DatePickerDialog(getContext(),
+                R.style.datePickerDialog,(view, year1, monthOfYear, dayOfMonth) -> {
+            String monthZero = String.format("%02d", (monthOfYear + 1));
+            String dayZero = String.format("%02d", dayOfMonth);
+            String selectedDate =  year1+"-"+ monthZero + "-" +dayZero;
+            binding.dateEditText.setText(selectedDate);
+        }, year, month, day);
+
+        binding.dateEditText.setOnClickListener(v -> datePickerDialogBirthday.show());
+        binding.dateEditText.setInputType(InputType.TYPE_NULL);
+    }
+
 }
---
+++ app/src/main/java/com/medicapp/medicappprojectcomp/utils/HoraTextWatcher.java
@@ -0,0 +1,21 @@
+package com.medicapp.medicappprojectcomp.utils;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+ public class HoraTextWatcher implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // No se necesita implementar
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // No se necesita implementar
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        // No se necesita implementar
+    }
+}
